bfbcee12c589368d538d0a093fa6b914c72fecac
Geoff Crittenden <geoffcritt@hotmail.com>
2013-09-14 13:50:49 -0500

    Added search functions

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,14 @@
-require_relative '../lib/sunlight_legislators_importer.rb'
+# require_relative '../lib/sunlight_legislators_importer.rb'
+require_relative 'models/legislator'
+
+def get_state_legislators
+  Legislator.all.each { |legislator| puts "#{legislator.title} #{legislator.firstname} #{legislator.lastname}" if legislator.state == ARGV[0] }
+end
+
+# get_state_legislators
+
+def get_by_party
+  Legislator.all.each { |legislator| puts "#{legislator.title} #{legislator.firstname} #{legislator.lastname}, #{legislator.state} #{legislator.district}" if legislator.party == ARGV[0] && legislator.title == 'Rep' }  
+end
+
+get_by_party
